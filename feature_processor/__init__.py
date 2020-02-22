80418603bf46675c9585fbae782de5d799c47a5f
Artem Baraniuk <artem.baranyuk@gmail.com>
2020-02-22 19:49:33 +0100

    Base for Feature processor module added

---
+++ feature_processor/__init__.py
@@ -0,0 +1,2 @@
+from .statistics_functions import merge_statistics, update_statistics
+from .feature_functions import *
---
+++ feature_processor/feature_functions.py
@@ -0,0 +1,14 @@
+def calculate_z_score(features, means, stds):
+    raise NotImplementedError()
+
+def get_argmax(features):
+    raise NotImplementedError()
+
+def calculate_abs_max_mean_diff(features, means):
+    raise NotImplementedError()
+
+def get_min(features):
+    raise NotImplementedError()
+
+def get_max(features):
+    raise NotImplementedError()
